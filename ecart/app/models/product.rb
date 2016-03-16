2f5ec652134691c0e9c3f5e18fe5e7dfb34ae1d1
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-03-15 22:31:02 -0500

    Added category and user models and created references

--- ecart/app/models/product.rb
+++ ecart/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
-has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>" }
+	belongs_to :category
+has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>" , large: "750x500"}
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- ecart/db/schema.rb
+++ ecart/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314221123) do
+ActiveRecord::Schema.define(version: 20160316025901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +33,18 @@ ActiveRecord::Schema.define(version: 20160314221123) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "category_id"
   end
 
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.boolean  "type",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_foreign_key "products", "categories"
 end
